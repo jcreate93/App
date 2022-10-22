56481c9019abc8f319f80ee1a5b8690b5263401f
jcreate93 <chambj22@ferris.edu>
2022-10-22 10:11:26 -0400

    initial commit

---
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.app.databinding.ActivityMainBinding;
+
+public class MainActivity extends Activity {
+
+    private TextView mTextView;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        mTextView = binding.text;
+    }
+}
